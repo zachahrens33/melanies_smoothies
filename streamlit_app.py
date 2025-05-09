2d0869d413e896d561e351a2f0541c8be9bcadf7
zachahrens33 <107057426+zachahrens33@users.noreply.github.com>
2025-05-09 13:43:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,4 +66,4 @@ st.write("https://github.com/appuv")
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(date=smoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
