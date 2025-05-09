cfc969d52c25aa4f93b66699657e4b40dcf804e0
zachahrens33 <107057426+zachahrens33@users.noreply.github.com>
2025-05-09 14:25:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ try:
     session = cnx.session()
 
     # Retrieve fruit options from Snowflake
-    my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+    my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
     st.dataframe(data=my_dataframe, use_container_width=True)
     st.stop()
 
