8d7b6efbc86a06d8d95bf7e228c781c231798106
zachahrens33 <107057426+zachahrens33@users.noreply.github.com>
2025-05-09 12:31:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+st.write(st.secrets)
+
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw: {st.__version__}")
 st.write(
