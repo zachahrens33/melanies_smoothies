9d964e2a192f7fe0916fc07171f62d2481f58203
zachahrens33 <107057426+zachahrens33@users.noreply.github.com>
2025-05-09 13:43:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,5 +64,6 @@ except Exception as ex:
 st.write("https://github.com/appuv")
 
 import requests
-# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(date=smoothiefroot_response.json(), use_container_width=True)
