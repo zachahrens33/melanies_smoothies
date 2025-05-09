aba9195bfc92fe8523b75185b31df251959e0f49
zachahrens33 <107057426+zachahrens33@users.noreply.github.com>
2025-05-09 14:43:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,10 +41,10 @@ try:
             try:
                 # Make API request to get details about each fruit
                 search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-                st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+                #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
                 
                 st.subheader(fruit_chosen + "Nutritional Information")
-                smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+                smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
                 smoothiefroot_response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
                 
                 if smoothiefroot_response.status_code == 200:
