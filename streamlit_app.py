ce08fcad9990834558e0e7615db4c478093b3d70
zachahrens33 <107057426+zachahrens33@users.noreply.github.com>
2025-05-09 13:40:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,4 +65,4 @@ st.write("https://github.com/appuv")
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
